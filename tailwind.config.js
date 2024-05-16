7fc17d639575ee2ae79ac3b63a6fd779f73a8a03
Movicks <codlabx@gmail.com>
2024-05-16 12:15:25 +0100

    first commit from Victors

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+// const flowbite = require("flowbite-react/tailwind");
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
